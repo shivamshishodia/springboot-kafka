b756140fd9e41d25fe4f7ca67d40b0c4b4573e11
Shivam Shishodia <theshishodia@gmail.com>
2022-01-31 21:54:50 +0530

    Initial library-events-producer commit

---
+++ library-events-producer/src/main/java/com/shishodia/kafka/libraryeventsproducer/LibraryEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.shishodia.kafka.libraryeventsproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventsProducerApplication.class, args);
+	}
+
+}
